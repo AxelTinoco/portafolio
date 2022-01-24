ed18529827fd15e28649f35737d66b7a7a0b64ad
AxelTinoco <axeltm8@gmail.com>
2022-01-23 18:40:12 -0600

    Fix links

--- src/components/AnimatedDiv.js
+++ src/components/AnimatedDiv.js
@@ -53,7 +53,7 @@ export const AnimatedDiv = ({ id, title, description, link}) => {
                         <p className='text-white inline-flex w-9/12'>{description}</p>
 
                         <button className="bg-white rounded-2xl text-black w-[25%] h-12">
-                           <a href={link} target="_blank" rel="noopener noreferrer">Visit</a> 
+                           <a href={link}>Visit</a> 
                         </button>
                     </div>
                     :
--- src/components/Social.js
+++ src/components/Social.js
@@ -6,10 +6,10 @@ export const Social = () => {
     return (
         <div className='md:flex text-white flex-row w-full space-x-4 text-2xl pt-3  my-6 z-50 hidden'>
             <h3 className="opacity-50">Follow me!</h3>
-            <a href="https://github.com/AxelTinoco" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/AxelTinoco">
                 <ImGithub className=' opacity-50 hover:opacity-100 cursor-pointer'/>
             </a>
-            <a href="https://www.linkedin.com/in/axeltinoco/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/axeltinoco/">
             <ImLinkedin className=' opacity-50 hover:opacity-100 cursor-pointer'/>
             </a>
         </div>
